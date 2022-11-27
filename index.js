2f286b8801b2c9e7849994af33427dbb5208e314
KatePann <beljakova-kat@yandex.ru>
2022-11-27 09:47:22 +0300

    status,del

--- index.js
+++ index.js
@@ -2,35 +2,84 @@ const form = document.querySelector('#form');
 const newTask = document.querySelector('#new');
 const btn =  document.querySelector('#add');
 const tasks = document.querySelector('#tasks');
-console.log(newTask, add, tasks, form);
+const count = document.querySelector('#count');
 
 
 let todoList = [];
 
+// if (localStorage.getItem('todo')) {
+//     todoList = JSON.parse(localStorage.getItem('todo'))
+//     addTask();
+// }
+
 btn.addEventListener('click', function(){
     let newTodo = {
+        id: Date.now(),
         todo: newTask.value,
         checked: false,
-        important: false,
     }
-    todoList.push(newTodo)
+    todoList.push(newTodo);
+    newTask.value='';
     messages();
+    // localStorage.setItem('todo', JSON.stringify(todoList));
 })
 
+//добавление задачи
+
 function messages() {
-    let displayMessage ='';
+    let addTask ='';
 
     todoList.forEach(function(item,index) {
-         displayMessage += `
-                <div class="todo__task">
-                    <label  class="todo__checkbox">
-                        <input type="checkbox">
-                        <div></div>
-                    </label>
-                    <ul id="tasks" class="todo__task-text"><li>${newTask.value}</li></ul>
-                    <div class="todo__task-del">✗</div>
-                </div>
+        const cls = item.checked ? 'todo__task todo__task_complete' : 'todo__task';
+        const checked = item.checked ? 'checked' : '';
+
+        addTask += `
+            <div id='${item.id}' class="${cls}">
+                <label class="todo__checkbox">
+                    <input type="checkbox" ${checked}>
+                    <div class="todo__checkbox-div"></div>
+                </label>
+                <div class="todo__task-text">${item.todo}</div>
+                <div class="todo__task-del">✗</div>
+            </div>
         `;
-        tasks.innerHTML = displayMessage;
+        tasks.innerHTML = addTask;
     })
 }
+//изменение статуса задачи
+
+tasks.addEventListener('click', function (event) {
+    const target = event.target
+    const isCheckboxEl = target.classList.contains('todo__checkbox-div')
+    const isDeleteEl = target.classList.contains('todo__task-del')
+
+    if (isCheckboxEl) {
+      const task = target.parentElement.parentElement
+      const taskId = task.getAttribute('id')
+      changeTaskStatus(taskId, todoList);
+     
+    }
+    if(isDeleteEl){
+      const task = target.parentElement
+      const taskId = task.getAttribute('id')
+      deleteTask(taskId, todoList)
+    }
+})
+
+function changeTaskStatus(id, list) {
+    list.forEach((item)=>{
+        if(item.id == id){
+            item.checked = !item.checked
+        }
+    })
+}
+
+//удаление задачи
+function deleteTask(id, list) {
+    list.forEach((item, index)=>{
+        if(item.id == id){
+            list.splice(index,1)
+        }
+    })
+}
+
