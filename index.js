af2d6c06283535cf03bb7c48a8dedd98df867809
KatePann <beljakova-kat@yandex.ru>
2022-12-01 07:26:59 +0300

    правки

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const form = document.querySelector('#form');
 const newTask = document.querySelector('#new');
 const btn =  document.querySelector('#add');
 const tasks = document.querySelector('#tasks');
 const pendingNum = document.querySelector('.pending__num')
 const clearButton = document.querySelector('.clear__button')
+const filterOption = document.querySelector('.filter__todo')
+
 
 
 
@@ -18,6 +19,8 @@ if (localStorage.getItem('change')) {
     messages();
 }
 
+//добавление задачи
+
 btn.addEventListener('click', function(){
     let newTodo = {
         id: Date.now(),
@@ -29,8 +32,6 @@ btn.addEventListener('click', function(){
     messages();
     localStorage.setItem('todo', JSON.stringify(todoList));
 })
-
-//добавление задачи
 
 function messages() {
     let addTask ='';
@@ -46,59 +47,69 @@ function messages() {
                     <div class="todo__checkbox-div"></div>
                 </label>
                 <div class="todo__task-text">${item.todo}</div>
-                <div class="todo__task-del">✗</div>
+                <div data-action="delete" class="todo__task-del">✗</div>
             </div>
         `;
         tasks.innerHTML = addTask;
         allTasks()
     })
 }
-//изменение статуса задачи
 
-tasks.addEventListener('click', function (event) {
-    const target = event.target
-    const isCheckboxEl = target.classList.contains('todo__checkbox-div')
-    const isDeleteEl = target.classList.contains('todo__task-del')
+//изменение статуса задачи и удаление
 
-    if (isCheckboxEl) {
-      const task = target.parentElement.parentElement
-      const taskId = task.getAttribute('id')
-      changeTaskStatus(taskId, todoList);
-      messages()
-    }
+// tasks.addEventListener('click', function (event) {
+//     const target = event.target
+//     const isCheckboxEl = target.classList.contains('todo__checkbox-div')
+//     const isDeleteEl = target.classList.contains('todo__task-del')
+
+//     if (isCheckboxEl) {
+//       const task = target.parentElement.parentElement
+//       const taskId = task.getAttribute('id')
+//       changeTaskStatus(taskId, todoList);
+//       messages()
+//     }
     
-    if(isDeleteEl){
-      const task = target.parentElement
-      const taskId = task.getAttribute('id')
-      deleteTask(taskId, todoList)
-    }
-      messages()
-      localStorage.setItem('change', JSON.stringify(todoList));
+//     if(isDeleteEl){
+//       const task = target.parentElement
+//       const taskId = task.getAttribute('id')
+//       deleteTask(taskId, todoList)
+//     }
+//       messages()
+//       localStorage.setItem('change', JSON.stringify(todoList));
+// })
+
+// function changeTaskStatus(id, list) {
+//     list.forEach((item)=>{
+//         if(item.id == id){
+//             item.checked = !item.checked
+//         }
+//     })
+// }
+
+// function deleteTask(id, list) {
+//     list.forEach((item, index)=>{
+//         if(item.id == id){
+//             list.splice(index,1)
+//         }
+//     })
+// }
+
+tasks.addEventListener('click', function (event){
+if (event.target.dataset.action==='delete') {
+    const parentNode = event.target.closest('.pending')
+    parentNode.remove()
+}
 })
 
-function changeTaskStatus(id, list) {
-    list.forEach((item)=>{
-        if(item.id == id){
-            item.checked = !item.checked
-        }
-    })
-}
 
-//удаление задачи
-function deleteTask(id, list) {
-    list.forEach((item, index)=>{
-        if(item.id == id){
-            list.splice(index,1)
-        }
-    })
-}
+
 
 //редактирование задачи
 tasks.addEventListener('dblclick', (e) => {
-    if (e.target.textContent) {
-        const newText = prompt();
-      e.target.textContent = newText;
-    }
+    console.log(e);
+    // e.target.textContent
+
+    
   });
 
   //инфо
@@ -108,6 +119,36 @@ tasks.addEventListener('dblclick', (e) => {
   }
 
   clearButton.addEventListener('click', ()=>{
-    tasks.innerHTML=''
+    tasks.remove()
     allTasks()
   })
+
+
+  filterOption.addEventListener('click', filterTodo)
+  
+  function filterTodo(e) {
+      const todos= tasks.childNodes
+      console.log(todos);
+    todos.forEach(function (todo) {
+        console.log(todo);
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = 'flex' 
+                break;
+            case "active":
+                if (todo.classList.contains('active')) {
+                   todo.style.display= 'flex' 
+                }else{
+                    todo.style.display= 'none'
+                }
+                break;
+            case "completed":
+                    if (!todo.classList.contains('active')) {
+                       todo.style.display= 'flex' 
+                    }else{
+                        todo.style.display= 'none'
+                    }
+                break;
+        }
+    })
+  }
